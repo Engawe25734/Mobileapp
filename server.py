1aae4e8f374b59d21329e729cb5e641824927546
Engawe25734 <95743057+Engawe25734@users.noreply.github.com>
2026-07-23 22:55:28 -0400

    Update server.py

--- server.py
+++ server.py
@@ -114,10 +114,9 @@ initialize_database()
 async def home(request: Request):
 
     return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request
-        }
+        request=request,
+        name="index.html",
+        context={}
     )
 
 
